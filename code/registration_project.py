e408e7a5f1f1f1fc69e83581c5161fb47c8b9dee
ruzzlamb-cmd <ruzzlamb@gmail.com>
2026-05-12 15:39:50 +0200

    code voor project werkt nog niet helemaal

--- code/registration_project.py
+++ code/registration_project.py
@@ -33,7 +33,7 @@ def intensity_based_registration_demo():
     fun = lambda x: reg.rigid_corr(I, Im, x, return_transform=False)
 
     # the learning rate
-    mu = 0.001
+    mu = 0.0005 #iets kleiner dan 0.001, anders divergeert het in lokaal minimum
 
     # number of iterations
     num_iter = 200
@@ -88,3 +88,70 @@ def intensity_based_registration_demo():
         ax2.autoscale_view()
         fig.canvas.draw()
         display(fig)
+
+
+def compare_ncc_mi_registration():
+    # Fixed = T1, Moving = T2
+    I = plt.imread('../data/image_data/3_1_t1.tif')
+    Im = plt.imread('../data/image_data/3_1_t2.tif')
+
+    # affine parameters:
+    # [rotation, sx, sy, shearx, sheary, tx, ty]
+    x_ncc = np.array([0., 1., 1., 0., 0., 0., 0.])
+    x_mi  = np.array([0., 1., 1., 0., 0., 0., 0.])
+
+    fun_ncc = lambda x: reg.affine_corr(I, Im, x, return_transform=False)
+    fun_mi  = lambda x: reg.affine_mi(I, Im, x, return_transform=False)
+
+    mu_ncc = 0.001
+    mu_mi = 0.001
+
+    num_iter = 2
+
+    sim_ncc = []
+    sim_mi = []
+
+    for k in range(num_iter):
+        # Sla waarde op VOOR de update (geen extra aanroep nodig)
+        sim_ncc.append(fun_ncc(x_ncc))
+        sim_mi.append(fun_mi(x_mi))
+
+        # NCC optimization
+        g_ncc = reg.ngradient(fun_ncc, x_ncc)
+        x_ncc += mu_ncc * g_ncc
+
+        # MI optimization
+        g_mi = reg.ngradient(fun_mi, x_mi)
+        x_mi += mu_mi * g_mi
+
+    # final transformed images
+    _, Im_ncc, _ = reg.affine_corr(I, Im, x_ncc)
+    _, Im_mi, _ = reg.affine_mi(I, Im, x_mi)
+
+    # visualization
+    fig = plt.figure(figsize=(15,8))
+
+    ax1 = fig.add_subplot(231)
+    ax1.imshow(I, cmap='gray')
+    ax1.set_title("Fixed T1")
+
+    ax2 = fig.add_subplot(232)
+    ax2.imshow(I, cmap='gray')
+    ax2.imshow(Im_ncc, cmap='hot', alpha=0.5)
+    ax2.set_title("NCC Registration")
+
+    ax3 = fig.add_subplot(233)
+    ax3.imshow(I, cmap='gray')
+    ax3.imshow(Im_mi, cmap='hot', alpha=0.5)
+    ax3.set_title("MI Registration")
+
+    ax4 = fig.add_subplot(212)
+    ax4.plot(sim_ncc, label='NCC')
+    ax4.plot(sim_mi, label='MI')
+    ax4.set_xlabel("Iteration")
+    ax4.set_ylabel("Similarity")
+    ax4.legend()
+    ax4.grid()
+
+    plt.tight_layout()
+    plt.show()
